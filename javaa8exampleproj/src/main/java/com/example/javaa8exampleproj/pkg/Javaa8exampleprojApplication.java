fd284351b879a1cc149313849fe96f5fc2193c91
prparamesh <parameshwar507@gmail.com>
2022-12-18 21:56:45 +0530

    Sample project for java

---
+++ javaa8exampleproj/src/main/java/com/example/javaa8exampleproj/pkg/Javaa8exampleprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaa8exampleproj.pkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaa8exampleprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javaa8exampleprojApplication.class, args);
+	}
+
+}
---
+++ javaa8exampleproj/src/test/java/com/example/javaa8exampleproj/pkg/Javaa8exampleprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaa8exampleproj.pkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javaa8exampleprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
